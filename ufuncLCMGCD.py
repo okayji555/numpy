98305bc5a23cfff0516a4348b48270ffe84d3d1b
Omkar Gore <yothisisokg@gmail.com>
2026-05-11 01:34:41 +0530

    Update and rename ufuncLCM.py to ufuncLCMGCD.py

--- ufuncLCMGCD.py
+++ ufuncLCMGCD.py
@@ -7,11 +7,22 @@ arr = np.array([3, 6, 9])
 
 arr1 = np.arange(1,11)
 
+arr2 = np.array([20, 8, 32, 36, 16])
+
+
+#LCM
 lcm = np.lcm(num1, num2)
 x = np.lcm.reduce(arr)
 # reduce takes the function and collapses it into a single value
 y = np.lcm.reduce(arr1)
 
+#GCD
+b = np.gcd(num1, num2)
+a = np.gcd.reduce(arr2)
+
+
 print(lcm)
 print(x)
 print(y)
+print(a)
+print(b)
